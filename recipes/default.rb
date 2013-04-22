5e09ebe201d2e9c524a93ec44de965070658a48a
KONRAD Frederic <fred.konrad@greensocs.com>
2013-04-22 19:36:13 +0200

    recipes: take the rebased version.
    This take the new rebased version.
    
    Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,7 +34,7 @@ bash "Checkout QEMU SYSTEMC" do
   for i in #{node[:prefix]}/bash.profile.d/*; do . $i; done
 
 # need to specify branch
-    git clone git://git.greensocs.com/qemu-sc.git -b new_system_c  #{node[:prefix]}/ModelLibrary/greensocs/qemu_sc.source
+    git clone git://git.greensocs.com/qemu-sc.git -b new_system_c_rebased  #{node[:prefix]}/ModelLibrary/greensocs/qemu_sc.source
   EOH
   creates "#{node[:prefix]}/ModelLibrary/greensocs/qemu_sc.source"
   environment ({ 'http_proxy' => Chef::Config[:http_proxy] })
