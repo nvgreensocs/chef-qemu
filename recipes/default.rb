27ee8b999087cdbb48c7edd19910960b5dc72eda
KONRAD Frederic <fred.konrad@greensocs.com>
2013-04-22 19:36:44 +0200

    recipes: enable virtfs.
    Enable virtfs, to compile virtio-9p and have a shared directory
    between host and guest.
    
    Signed-off-by: KONRAD Frederic <fred.konrad@greensocs.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -47,7 +47,7 @@ ruby_block "compile QEMU SystemC" do
        # the profile should now include SystemC export SYSTEMC_HOME=/usr/local/systemc-2.3.0
 
        cd #{node[:prefix]}/ModelLibrary/greensocs/qemu_sc.source
-       ./configure --prefix=#{node[:prefix]}/ModelLibrary/greensocs --target-list=arm-softmmu --enable-fdt --enable-debug --disable-pie --enable-sdl --with-greensocs=#{node[:prefix]}/ModelLibrary/greensocs --with-systemc=$SYSTEMC_HOME --with-tlm=$SYSTEMC_HOME --with-boost=/usr
+       ./configure --prefix=#{node[:prefix]}/ModelLibrary/greensocs --target-list=arm-softmmu --enable-fdt --enable-virtfs --enable-debug --disable-pie --enable-sdl --with-greensocs=#{node[:prefix]}/ModelLibrary/greensocs --with-systemc=$SYSTEMC_HOME --with-tlm=$SYSTEMC_HOME --with-boost=/usr
        make
        make install
      EOH
